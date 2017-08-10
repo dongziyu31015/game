3138b1e2ef33439ac753d185c4f61003ccecab52
dongziyu31015 <dongziyu31015@gmail.com>
2017-08-10 02:15:12 -0400

    first commit

---
+++ static/pong/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1502085139,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bara-sheet0.png",
+		"images/barb-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/addscorea-sheet0.png",
+		"images/texta.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
